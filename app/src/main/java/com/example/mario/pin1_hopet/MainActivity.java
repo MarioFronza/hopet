4cbc3cae46e33eefc561b651c3406c4f22ca3de1
MarioFronza <mariofronza1@gmail.com>
2018-10-14 17:08:32 -0300

    tela de cadastro e login

--- app/src/main/java/com/example/mario/pin1_hopet/MainActivity.java
+++ app/src/main/java/com/example/mario/pin1_hopet/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.mario.pin1_hopet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
